0f147173345a3df4eabbcccae56965c504106db9
chrou <chrou@grandstream.cn>
2021-09-18 14:35:22 +0800

    first commit: add source

---
+++ README.md
@@ -0,0 +1,22 @@
+
+This is a repository for the WebRTC JavaScript code samples for learning.
+
+---------
+
+## Standard documentation link
+
+[WebRTC API](https://developer.mozilla.org/en-US/docs/Web/API/WebRTC_API)
+
+[WebRTC 1.0: Real-Time Communication Between Browsers](https://www.w3.org/TR/webrtc/)
+
+[Session Description Protocol (SDP) Parameters](https://www.iana.org/assignments/sdp-parameters/sdp-parameters.xhtml)
+
+[Session Initiation Protocol (SIP) Parameters](https://www.iana.org/assignments/sip-parameters/sip-parameters.xhtml)
+
+### RFC
+
+[RFC 4566: Session Description Protocol(SDP)](https://datatracker.ietf.org/doc/html/rfc4566)
+
+[RFC 3261: Session Initiation Protocol(SIP)](https://datatracker.ietf.org/doc/html/rfc3261)
+
+[RFC 8829: JavaScript Session Establishment Protocol (JSEP)](https://www.rfc-editor.org/rfc/rfc8829.pdf)
---
+++ src/content/codec/bandwidthAndRembTransportCC/index.js
@@ -0,0 +1,305 @@
+'use strict';
+
+let getMediaButton = document.querySelector('button#getMedia');
+let connectButton = document.querySelector('button#connect');
+let hangupButton = document.querySelector('button#hangup');
+
+getMediaButton.onclick = getMedia;
+connectButton.onclick = createPeerConnection;
+hangupButton.onclick = hangup;
+
+let local_framesPerSecond = document.querySelector('div#local_framesPerSecond');
+let remote_framesPerSecond = document.querySelector('div#remote_framesPerSecond');
+let bitrateDiv = document.querySelector('div#bitrate');
+let peerDiv = document.querySelector('div#peer');
+let senderStatsDiv = document.querySelector('div#senderStats');
+let receiverStatsDiv = document.querySelector('div#receiverStats');
+
+let localVideo = document.querySelector('div#localVideo video');
+let remoteVideo = document.querySelector('div#remoteVideo video');
+let localVideoStatsDiv = document.querySelector('div#localVideo div');
+let remoteVideoStatsDiv = document.querySelector('div#remoteVideo div');
+
+let localPeerConnection;
+let remotePeerConnection;
+let localStream;
+let remoteStream;
+let bytesPrev;
+let timestampPrev;
+
+function createPeerConnection() {
+    console.info("begin create peerConnections");
+    connectButton.disabled = true;
+    hangupButton.disabled = false;
+
+    bytesPrev = 0;
+    timestampPrev = 0;
+    localPeerConnection = new RTCPeerConnection(null);
+    remotePeerConnection = new RTCPeerConnection(null);
+
+    if(localStream){
+        localStream.getTracks().forEach(
+            function(track) {
+                console.info("localPeerConnection addTack!");
+                localPeerConnection.addTrack(track, localStream);
+            }
+        );
+    }
+    if(remoteStream){
+        remoteStream.getTracks().forEach(
+            function(track) {
+                console.info("localPeerConnection addTack!");
+                remotePeerConnection.addTrack(track, remoteStream);
+            }
+        );
+    }
+
+    console.info('localPeerConnection creating offer');
+    localPeerConnection.onnegotiationeeded = function() {
+        console.info('Negotiation needed - localPeerConnection');
+    };
+    remotePeerConnection.onnegotiationeeded = function() {
+        console.info('Negotiation needed - remotePeerConnection');
+    };
+    localPeerConnection.onicecandidate = function(e) {
+        console.info('Candidate localPeerConnection');
+        remotePeerConnection.addIceCandidate(e.candidate).then(onAddIceCandidateSuccess, onAddIceCandidateError);
+    };
+    remotePeerConnection.onicecandidate = function(e) {
+        console.info('Candidate remotePeerConnection');
+        localPeerConnection.addIceCandidate(e.candidate).then(onAddIceCandidateSuccess, onAddIceCandidateError);
+    };
+
+    localPeerConnection.ontrack = function(e) {
+        if (localVideo.srcObject !== e.streams[0]) {
+            console.info('localPeerConnection got stream ' + e.streams[0].id);
+            localVideo.srcObject = e.streams[0];
+        }
+    };
+
+    remotePeerConnection.ontrack = function(e) {
+        if (remoteVideo.srcObject !== e.streams[0]) {
+            console.info('remotePeerConnection got stream ' + e.streams[0].id);
+            remoteVideo.srcObject = e.streams[0];
+        }
+    };
+
+    localPeerConnection.createOffer().then(
+        function(offer) {
+            console.info('localPeerConnection setLocalDescription:\n', offer.sdp);
+            localPeerConnection.setLocalDescription(offer).then(function () {
+                console.info('setLocalDescription success');
+            }).catch(function (error) {
+                console.error(error)
+            })
+
+            offer.sdp = decorateSdp(offer.sdp)
+            console.info(`remotePeerConnection setRemoteDescription : \n${offer.sdp}`);
+            remotePeerConnection.setRemoteDescription(offer).then(function () {
+                console.info('remotePeerConnection setRemoteDescription success')
+            }).catch(function (err) {
+                console.error(err)
+            })
+
+            remotePeerConnection.createAnswer().then(
+                function(answer) {
+                    console.info('remotePeerConnection setLocalDescription: \n', answer.sdp);
+                    remotePeerConnection.setLocalDescription(answer).then(function () {
+                        console.info('setLocalDescription success')
+                    }).catch(function (err) {
+                        console.error(err)
+                    })
+
+                    answer.sdp = decorateSdp(answer.sdp)
+                    console.warn(`localPeerConnection setRemoteDescription:\n${answer.sdp}`);
+                    localPeerConnection.setRemoteDescription(answer).then(function () {
+                        console.info('localPeerConnection setRemoteDescription success')
+                    }).catch(function (err) {
+                        console.error(err)
+                    })
+                },
+                function(err) {console.info(err);}
+            );
+        },
+        function(err) {console.info(err);}
+    );
+}
+
+function onAddIceCandidateSuccess() {
+    console.info('AddIceCandidate success.');
+}
+
+function onAddIceCandidateError(error) {
+    console.info('Failed to add Ice Candidate: ' + error.toString());
+}
+
+function hangup() {
+    console.info('Ending call');
+    localPeerConnection.close();
+    remotePeerConnection.close();
+    window.location.reload();
+
+    // query stats one last time.
+    Promise.all([
+        remotePeerConnection.getStats(null).then(showRemoteStats, function(err) {console.info(err);}),
+        localPeerConnection.getStats(null).then(showLocalStats, function(err) {console.info(err);})
+    ]).then(() => {
+        localPeerConnection = null;
+        remotePeerConnection = null;
+    });
+
+    localStream.getTracks().forEach(function(track) {track.stop();});
+    localStream = null;
+    hangupButton.disabled = true;
+    getMediaButton.disabled = false;
+}
+
+/************************************************* 取流部分 *************************************************************/
+var getUserMediaConstraintsDiv = document.querySelector('textarea#getUserMediaConstraints');
+var defaultCon = {
+    audio: false,
+    video: {
+        frameRate: 30,
+        aspectRatio: {
+            min: 1.777,
+            max: 1.778
+        },
+        width: 1280,
+        height: 720,
+    }
+};
+
+getUserMediaConstraintsDiv.value = JSON.stringify(defaultCon, null, '    ' );
+
+function getUsingDeviceId () {
+    var deviceId = ''
+    var selectedIndex = document.getElementById('videoList').options.selectedIndex
+    if(selectedIndex < 0){
+        selectedIndex = 0
+    }
+    var selectedOption = document.getElementById('videoList').options[selectedIndex]
+    if(selectedOption && selectedOption.value){
+        deviceId = selectedOption.value
+    }
+    console.log('get deviceId: ' + deviceId)
+    return deviceId
+}
+
+function selectDeviceAndGum(){
+    var deviceId = getUsingDeviceId()
+    console.warn("deviceId: ", deviceId)
+    if(deviceId === ""){
+        console.warn("请选择有效设备")
+        return
+    }
+    getMedia()
+}
+
+function getMedia() {
+    console.warn('GetUserMedia start!');
+    console.log("clear stream first")
+    closeStream()
+    getMediaButton.disabled = true;
+
+    var constraints = JSON.parse(getUserMediaConstraintsDiv.value)
+    var deviceId = getUsingDeviceId()
+    if(deviceId){
+        constraints.video.deviceId = deviceId
+    }
+
+    console.warn("getNewStream constraint: \n" + JSON.stringify(constraints, null, '    ') );
+    navigator.mediaDevices.getUserMedia(constraints).then(function (stream){
+        console.warn('get local media stream succeeded:' , stream.id);
+        connectButton.disabled = false;
+        localStream = stream;
+        localVideo.srcObject = stream;
+
+        // // 获取远端流
+        // var selectedIndex = document.getElementById('videoList').options.selectedIndex
+        // var options = document.getElementById('videoList').options
+        // if(selectedIndex){
+        //     for(var i = 0; i<options.length; i++){
+        //         if(i !== selectedIndex){
+        //             deviceId = options[i].value
+        //         }
+        //     }
+        // }else {
+        //     deviceId = options[options.length-1].value
+        // }
+        // console.warn("deviceId: ", deviceId)
+        // if(deviceId){
+        //     constraints.video.deviceId = deviceId
+        // }
+        // navigator.mediaDevices.getUserMedia(constraints).then(function (_stream){
+        //     console.warn('get remote media stream succeeded:', _stream.id);
+        //     remoteStream = _stream
+        //     remoteVideo.srcObject = _stream;
+        // }).catch(function(e) {
+        //     console.error(e)
+        //     console.warn("get remote media stream failed!");
+        // });
+    }).catch(function(e) {
+        console.error(e)
+        console.warn("getUserMedia failed!");
+        let message = 'getUserMedia error: ' + e.name + '\n' + 'PermissionDeniedError may mean invalid constraints.';
+        console.warn(message);
+        getMediaButton.disabled = false;
+    });
+}
+
+function closeStream() {
+    // clear first
+    var stream = localVideo.srcObject
+    if (stream){
+        try {
+            stream.oninactive = null;
+            var tracks = stream.getTracks();
+            for (var track in tracks) {
+                tracks[track].onended = null;
+                console.info("close stream");
+                tracks[track].stop();
+            }
+        }
+        catch (error) {
+            console.info('closeStream: Failed to close stream');
+            console.error(error);
+        }
+        stream = null;
+        localVideo.srcObject = null
+    }
+
+    if (localStream) {
+        localStream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+        var videoTracks = localStream.getVideoTracks();
+        for (var i = 0; i !== videoTracks.length; ++i) {
+            videoTracks[i].stop();
+        }
+    }
+}
+
+var mediaDeviceInstance
+document.onreadystatechange = function () {
+    if (document.readyState === "complete") {
+        mediaDeviceInstance = new MediaDevice()
+        var videoInputList = []
+        // videoInputList.push('<option class="cameraOption" value="">' + "请选择" + '</option>')
+        mediaDeviceInstance.enumDevices(deviceInfo => {
+            console.log('enumDevices' + JSON.stringify(deviceInfo.cameras))
+            if (deviceInfo.cameras) {
+                for (var j = 0; j < deviceInfo.cameras.length; j++) {
+                    if (!deviceInfo.cameras[j].label) {
+                        deviceInfo.cameras[j].label = 'camera' + j
+                    }
+                    videoInputList.push('<option class="cameraOption" value="' + deviceInfo.cameras[j].deviceId + '">' + deviceInfo.cameras[j].label + '</option>')
+                    console.log('camera: ' + deviceInfo.cameras[j].label)
+                }
+            }
+            document.getElementById('videoList').innerHTML = videoInputList.join('')
+            getMedia();
+        }, function (error) {
+            console.error('enum device error: ' + error)
+        })
+    }
+}
